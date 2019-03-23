8cd9845a1522421837e78e312035f38f6ef7761b
Ifor Waldo Williams <ifor@designedbywaldo.com>
2019-03-23 20:25:48 +0800

    Update background.js

--- background.js
+++ background.js
@@ -1,14 +1,19 @@
 chrome.runtime.onInstalled.addListener(() => {
+    let token = null;
+
     chrome.storage.sync.get("token", data => {
         if (!data || !data.token) {
             return alert("Please go to the options page to set your personal access token.");
         }
+
+        token = data.token;
     });
 
     chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
         const xhr = new XMLHttpRequest();
         if (request.action === "FETCH_STATE") {
             xhr.open("GET", "http://thyme.test/api/extension", true);
+            xhr.setRequestHeader("X-PAT", token);
             xhr.onreadystatechange = () => {
                 if (xhr.readyState === 4) {
                     if (xhr.status === 200) {
@@ -24,6 +29,7 @@ chrome.runtime.onInstalled.addListener(() => {
         if (request.action === "START_TIMER") {
             xhr.open("POST", "http://thyme.test/api/stints", true);
             xhr.setRequestHeader("Content-type", "application/json");
+            xhr.setRequestHeader("X-PAT", token);
             xhr.onreadystatechange = () => {
                 if (xhr.readyState === 4) {
                     if (xhr.status === 200) {

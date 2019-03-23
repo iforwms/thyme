45c7925d13d8ab64def3914223e461f6a835d9af
Ifor Waldo Williams <ifor@designedbywaldo.com>
2019-03-23 13:52:35 +0800

    updates

--- background.js
+++ background.js
@@ -1,23 +1,15 @@
 chrome.runtime.onInstalled.addListener(function() {
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://jsonplaceholder.typicode.com/users", true);
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4) {
-            localStorage.setItem("THYME_INIT", xhr.responseText);
-            console.log("%cres", "color: #3490dc; font-weight: bold", xhr.responseText);
+    chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+        if (request.action === "FETCH_STATE") {
+            var xhr = new XMLHttpRequest();
+            xhr.open("GET", "http://thyme.test/api/extension", true);
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState == 4) {
+                    sendResponse(xhr.responseText);
+                }
+            };
+            xhr.send();
         }
-    };
-    xhr.send();
-
-    chrome.runtime.onMessage.addListener(function(msg) {
-        console.log("message recieved from popup.js:" + msg);
+        return true;
     });
-
-    // chrome.runtime.sendMessage({
-    //     msg: "something_completed",
-    //     data: {
-    //         subject: "Loading",
-    //         content: "Just completed!"
-    //     }
-    // });
 });
--- popup.js
+++ popup.js
@@ -1,26 +1,50 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.msg === "something_completed") {
-        //  To do something
-        console.log(request.data.subject);
-        console.log(request.data.content);
-    }
-});
-
 window.addEventListener("DOMContentLoaded", function() {
-    const res = JSON.parse(localStorage.getItem("THYME_INIT"));
-    let list = document.getElementById("users");
-    // let options = res.map(user => ({ id: user.id, name: user.name }));
-    res.forEach(user => {
-        list.add(new Option(user.name, user.id));
-    });
+    chrome.runtime.sendMessage({ action: "FETCH_STATE" }, function(res) {
+        const response = JSON.parse(res);
 
-    // document.getElementById("JSON").innerText = options;
-    //
-    //     chrome.runtime.sendMessage({
-    //         msg: "something_completed",
-    //         data: {
-    //             subject: "Loading",
-    //             content: "Just completed!"
-    //         }
-    //     });
+        if (!response.timer_running) {
+            document.getElementById("running").innerHTML = `
+            <div style="display: flex; margin-bottom: 1em;">
+                <div style="flex: 1;">
+                    <h1>Project Name</h1>
+                    <h2>(Client Name)</h2>
+                    <h2>Task Name</h2>
+                </div>
+                <div style="margin-top: auto;">
+                    <div>0:00</div>
+                    <div>Started at 12:34</div>
+                </div>
+            </div>
+            <div class="button-container">
+                <button id="cancel" class="btn btn-primary">Stop Timer</button>
+            </div>
+            `;
+            document.getElementById("thyme").classList.remove("loading");
+            document.getElementById("thyme").classList.add("running");
+            return;
+        }
+
+        let projectsList = document.getElementById("projects");
+        let tasksList = document.getElementById("tasks");
+
+        response.data.map(project => {
+            projectsList.add(new Option(project.project_name, project.id));
+        });
+
+        projectsList.addEventListener("change", e => {
+            const tasks = response.data.filter(
+                project => parseInt(project.id, 10) === parseInt(e.target.value, 10)
+            )[0].tasks;
+            tasksList.innerText = null;
+            tasks.map(task => {
+                tasksList.add(new Option(task.task_name, task.id));
+            });
+        });
+
+        document.getElementById("thyme").classList.remove("loading");
+
+        document.getElementById("submit").addEventListener("click", () => {
+            alert("clicked!");
+        });
+    });
 });
